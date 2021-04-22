05105be87599175f6eb6870f687fab97c1e65006
santoshwebinno <santosh.webinnovatorz@gmail.com>
2021-04-22 07:13:48 -0700

    add new code 22-04-2021

--- src/components/staking/pools.js
+++ src/components/staking/pools.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import listicon from '../../images/listicon.png';
 import gridicon from '../../images/gridicon.png';
@@ -6,6 +6,15 @@ import PoolsGrid from './poolsgrid';
 import PoolsList from './poolslist';
 
 export default function Pools(){
+    const [showGrid, setShowGrid] = useState(true);
+    const showhide = (ev, value) =>{
+        console.log(value);
+        if(value == 1){
+            setShowGrid(true)
+        } else {
+            setShowGrid(false)
+        }
+    }
 return (
     <Card className="myassets">
         <Row>
@@ -14,13 +23,15 @@ return (
         </Col>
         <Col md={6}>
         <Card className="pool_style_btn">
-            <a href="#"><img src={listicon}/></a>
-            <a href="#"><img src={gridicon}/></a>
+            <a href="javascript:void(0)" onClick={(ev => showhide(ev, 2))}><img src={listicon}/></a>
+            <a href="javascript:void(0)" onClick={(ev => showhide(ev, 1))}><img src={gridicon}/></a>
         </Card>
          </Col>   
         </Row>    
+        { showGrid ? 
         <PoolsGrid/>
-        <PoolsList/>
+        : 
+        <PoolsList/>}
     </Card>
 )
 
