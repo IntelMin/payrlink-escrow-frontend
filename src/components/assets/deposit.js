2123d9fbc2ccf3136cf6509c2d7926f8b34535ff
santoshwebinno <santosh.webinnovatorz@gmail.com>
2021-04-28 10:38:29 +0530

    add new code

--- src/components/assets/deposit.js
+++ src/components/assets/deposit.js
@@ -6,46 +6,42 @@ export default function Deposit(){
     return(
         <Card className="deposit_block">
             <h2>Deposit</h2>
-            <Form>
-                                
-                               
-                                  
-                                    <Form.Group controlId="Amount">
-                                    <Form.Label>Pool</Form.Label>
-                                    <Form.Control as="select" style={{ backgroundImage: `url(${bluedown})` }}>
-                                    <option>ETH</option>
-                                    <option>USDT</option>
-                                    <option>PAYR</option>
-                                    <option>BTC</option>
-                                    </Form.Control>
-                                    </Form.Group>
+            <Form>                  
+                <Form.Group controlId="Amount">
+                <Form.Label>Pool</Form.Label>
+                <Form.Control as="select" style={{ backgroundImage: `url(${bluedown})` }}>
+                <option>ETH</option>
+                <option>USDT</option>
+                <option>PAYR</option>
+                <option>BTC</option>
+                </Form.Control>
+                </Form.Group>
 
-                                    <Form.Group controlId="To">
-                                    <Form.Label>Amount</Form.Label>
-                                    <Form.Control type="text" />
-                                    <span className="max_value">Max: 2.2</span>
-                                    </Form.Group>
-                                    
-                                    <Card className="deposit_bottom" style={{ backgroundImage: `url(${depositbg})` }}>
-                                    <Table>
-                                        <tbody>
-                                            <tr>
-                                            <td>Current Balance</td>    
-                                            <td><strong>4.85 ETH</strong></td>
-                                            </tr>
-                                            <tr>
-                                            <td>Total Balance</td>    
-                                            <td><strong>8.08 ETH</strong></td>
-                                            </tr>
-                                            
-                                        </tbody>
-                                    </Table>
-                                        <p className="hide_text">.</p>
-                                    <Button variant="primary" type="submit">
-                                    Deposit
-                                    </Button>
-                                    </Card>
-                                
+                <Form.Group controlId="To">
+                <Form.Label>Amount</Form.Label>
+                <Form.Control type="text" />
+                <span className="max_value">Max: 2.2</span>
+                </Form.Group>
+                
+                <Card className="deposit_bottom" style={{ backgroundImage: `url(${depositbg})` }}>
+                <Table>
+                    <tbody>
+                        <tr>
+                        <td>Current Balance</td>    
+                        <td><strong>4.85 ETH</strong></td>
+                        </tr>
+                        <tr>
+                        <td>Total Balance</td>    
+                        <td><strong>8.08 ETH</strong></td>
+                        </tr>
+                        
+                    </tbody>
+                </Table>
+                    <p className="hide_text">.</p>
+                <Button variant="primary" type="submit">
+                Deposit
+                </Button>
+                </Card>                  
             </Form>
         </Card>
     )
